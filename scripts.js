e987e4965fc39891a4666d242cda882fb215ba07
David Vaughan <dvaug86@gmail.com>
2022-02-28 10:11:49 -0500

    button adds black squares

--- scripts.js
+++ scripts.js
@@ -2,34 +2,40 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
     let div4Btn = document.createElement('div'); //div to seperate the button from rest
-    div4Btn.className = div4Btn; //gives class name to div so that we know which one
+    div4Btn.className = 'div-4-Btn'; //gives class name to div so that we know which one
     document.body.appendChild(div4Btn); //adds the div to the body 
 
-
-
-    let button = document.createElement("button"); //creates a button
-    let buttonText = document.createTextNode("Add Square"); //text for said button
-    button.appendChild(buttonText); //adds the text to the button like a lable
-    div4Btn.body.appendChild(button); //adds the button to the button division
+    let btn = document.createElement("button"); //creates a button
+    let btnText = document.createTextNode("Add Square"); //text for said button
+    btn.appendChild(btnText); //adds the text to the button like a lable
+    div4Btn.appendChild(btn); //adds the button to the button division
 
 
 
     let sqContainer = document.createElement('div'); //container for the squares
-    sqContainer.className = 'sqContainer'; //gives the div of black square a class name
+    sqContainer.className = 'sq-Container'; //gives the div of black square a class name
     document.body.appendChild(sqContainer); //adds this container to the body
 
 
+    let bClick = 1; //count for labeling purposes
+    btn.addEventListener("click", function () {
+        let divSQ = document.createElement('div'); //this will be the division for each square
+        divSQ.className = 'blackSQ'; //allows work to be done in css
+        divSQ.id = (bClick); //allows each square to have an id starting at 1
+        
+        sqContainer.appendChild(divSQ); //Attaches this child to square container
+        let bClickText = document.createTextNode( bClick); //this provids a visual reference to the id
+        divSQ.appendChild(bClickText); //this puts the id# to each square
+        bClick++; //increases the id number each time the button is clicked
+    });
 
-    button.addEventListener("click", function () {
-        let divSquare = document.createElement('div'); //this will be the division for each square
-        let bClick = 0; //count for labeling purposes
+    div.addEventListener('mouseover', function () {
+        div.innerText = div.id;
 
-        sqContainer.appendChild(divSquare)
-
-
-    })
-
-
+    });
+    div.addEventListener('mouseout', function () {
+        div.innerText = '';
+    });
 
 });
 
