0dec25196d32a345ad4cfda451d4ee5a83a4a770
David Vaughan <dvaug86@gmail.com>
2022-03-02 05:49:19 -0500

    blocks show id when cursor hovers over them

--- scripts.js
+++ scripts.js
@@ -17,26 +17,20 @@ document.addEventListener("DOMContentLoaded", function () {
     document.body.appendChild(sqContainer); //adds this container to the body
 
 
-    let bClick = 1; //count for labeling purposes
+    let bClick = 0; //count for labeling purposes
     btn.addEventListener("click", function () {
+        bClick++; //increases the id number each time the button is clicked
+
         let divSQ = document.createElement('div'); //this will be the division for each square
         divSQ.className = 'blackSQ'; //allows work to be done in css
-        divSQ.id = (bClick); //allows each square to have an id starting at 1
-        
-        sqContainer.appendChild(divSQ); //Attaches this child to square container
-        let bClickText = document.createTextNode( bClick); //this provids a visual reference to the id
-        divSQ.appendChild(bClickText); //this puts the id# to each square
-        bClick++; //increases the id number each time the button is clicked
-    });
+        let bClickText = document.createTextNode(bClick); //this provids a visual reference to the id
+        divSQ.id = bClick; //allows each square to have an id starting at 1
+         sqContainer.appendChild(divSQ); //Attaches this child to square container
 
-    div.addEventListener('mouseover', function () {
-        div.innerText = div.id;
+        divSQ.addEventListener('mouseover', () => divSQ.appendChild(bClickText));
+        divSQ.addEventListener('mouseout', () => divSQ.removeChild(bClickText));
 
     });
-    div.addEventListener('mouseout', function () {
-        div.innerText = '';
-    });
-
 });
 
 
